9a04043f33a0049eeaa176f2bb896318c95f2bae
Facundo Herrera <facundhfed@gmail.com>
2024-03-18 11:44:16 +0100

    Trying again uploading vercel file

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -6,13 +6,7 @@ import { TodoRoutes } from "./todos/routes";
 export class AppRoutes {
     static get routes(): Router {
         const router = Router();
-
-        const todoController = new TodosController()
-
-      
          router.use('/api/todos', TodoRoutes.routes );
-       
-
         return router;
     }
 }
