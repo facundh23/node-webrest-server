5a520f3d0167651c033f829d812a5398d7977799
Facundo Herrera <facundhfed@gmail.com>
2024-03-18 14:05:49 +0100

    Public path changed

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -5,7 +5,7 @@ import { TodoRoutes } from "./todos/routes";
 export class AppRoutes {
     static get routes(): Router {
         const router = Router();
-         router.use('/api/todos', TodoRoutes.routes );
+        router.use('/api/todos', TodoRoutes.routes );
         return router;
     }
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -12,18 +12,19 @@ export class Server {
 
     private app = express();
     private readonly port: number;
-    private readonly public_path: string
+    private readonly publicPath: string
     private readonly routes:Router;
 
     constructor(options:Options){
         const {port, public_path = 'public', routes} = options;
         this.port = port;
-        this.public_path = public_path
+        this.publicPath = public_path
         this.routes = routes
     }
 
      async start(){
          //  this.app.use(cors())
+         console.log(this.routes)
          
          //  Middlewares
          // Cualquier peticion que haga va a pasar por este middleware y si viene el body lo va a serializar como un json (RAW)
@@ -32,7 +33,7 @@ export class Server {
          this.app.use(express.urlencoded({extended:true}));
          
         // Public Folders
-        this.app.use(express.static(this.public_path));
+        this.app.use(express.static(this.publicPath));
         // Routes
         this.app.use(this.routes)
 
@@ -40,7 +41,7 @@ export class Server {
         // Aca podemos interceptar todas la request y las reponses 
         this.app.get('*', (req, res) => {
             // Asi aseguro que al recargar mi pagina no se me va a romper la aplicacion
-            const indexPath = path.join(__dirname + `../../../${this.public_path}/index.html`);
+            const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
             res.sendFile(indexPath);
             return;
         })
